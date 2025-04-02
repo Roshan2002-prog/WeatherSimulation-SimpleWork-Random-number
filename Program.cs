5c864a7c96576d02979a0bf1c48f605fd2dedee9
thoma <thoma@REVELATION>
2025-04-03 04:14:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace WeatherStationSimulator
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter tge number of days to simulate");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] conditions = {"Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-20, 55);
+                weatherConditions[i] = conditions[random.Next(0, 4)];
+            }
+        }
+    }
+}
